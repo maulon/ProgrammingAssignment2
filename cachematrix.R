9e64521cad1d96e1993d1fc39f67ae9c758a8b4c
paul <paul.m.jones@gmail.com>
2014-12-21 14:06:30 +0000

    completed programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming
+## Programming Assignment 2: Lexical Scoping
+## Cache what may be a time consuming calculation
+## example console useage
+## > m <- matrix(nrow=2,data=c(4,2,7,6))    # matrix to test with
+## > cm <- makeCacheMatrix(m)               # cached matrix
+## > cacheSolve(cm)                         # calculate and show the result
+## > cacheSolve(cm)                         # prints "getting cached data"
+                                            # and the cached result
 
-## Write a short comment describing this function
+## Create a vector that contains NULL when the calculation hasn't been done
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Use the vector to cache the result of calculating the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
 }
